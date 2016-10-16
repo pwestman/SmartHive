b9f5f05234ef2ce2cd26fd1dfc96087f10c194fc
Yurii <senciv1996@gmail.com>
2016-10-15 21:09:42 -0400

    Toolbar added.

--- app/src/main/java/nottoobee/toobee/smarthive/MainActivity.java
+++ app/src/main/java/nottoobee/toobee/smarthive/MainActivity.java
@@ -1,7 +1,10 @@
 package nottoobee.toobee.smarthive;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        ActionBar ab = getSupportActionBar();
+
+        try {
+            ab.setDisplayShowHomeEnabled(true);
+            ab.setLogo(R.drawable.logo);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        ab.setDisplayUseLogoEnabled(true);
     }
-    //testing
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bee_menu, menu);
+        return true;
+    }
 }
