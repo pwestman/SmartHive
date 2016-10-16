5534cff5eae8ba771b338807c900b8bfb9b72d85
Paul Westman <westman.paul@gmail.com>
2016-10-15 20:18:00 -0400

    Added toolbar to Main Activity.

--- app/src/main/java/nottoobee/toobee/smarthive/SignInActivity.java
+++ app/src/main/java/nottoobee/toobee/smarthive/SignInActivity.java
@@ -1,11 +1,8 @@
 package nottoobee.toobee.smarthive;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -15,15 +12,5 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
