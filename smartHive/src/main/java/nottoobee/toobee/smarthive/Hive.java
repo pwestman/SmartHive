966da29597301f05afe5b05d5380761ec7c59462
Roberto Loja <robertoloja@gmail.com>
2016-11-15 04:51:33 -0500

    Added some documentation.

--- smartHive/src/main/java/nottoobee/toobee/smarthive/Hive.java
+++ smartHive/src/main/java/nottoobee/toobee/smarthive/Hive.java
@@ -2,19 +2,27 @@ package nottoobee.toobee.smarthive;
 
 import java.util.Date;
 
+/*
+    N.B.: Instances of both of these classes can be passed directly to Firebase's setValue() method,
+    since they use the same field names as the database.
+
+    e.g.
+        ref.push().setValue(new Hive("Hive 1", "gps coords"));
+*/
+
 public class Hive {
     private String name;
     private String location;
-    private Date dateCreated;
+    private Date date_created;
     private Data[] data;
 
     public Hive() {
     }
 
-    public Hive(String name, String location, Date dateCreated) {
+    public Hive(String name, String location) {
         this.name = name;
         this.location = location;
-        this.dateCreated = dateCreated;
+        this.date_created = new Date();
     }
 
     public String getName() {
@@ -25,8 +33,8 @@ public class Hive {
         return location;
     }
 
-    public Date getDateCreated() {
-        return dateCreated;
+    public Date getDate_created() {
+        return date_created;
     }
 
     public Data[] getData() {
@@ -41,6 +49,17 @@ class Data {
     private int population;
     private int weight;
 
+    public Data() {
+    }
+
+    public Data(Date date, int humidity, int temperature, int population, int weight) {
+        this.date = date;
+        this.humidity = humidity;
+        this.temperature = temperature;
+        this.population = population;
+        this.weight = weight;
+    }
+
     public Date getDate() {
         return date;
     }
@@ -50,7 +69,7 @@ class Data {
     }
 
     public int getTemperature() {
-        return humidity;
+        return temperature;
     }
 
     public int getPopulation() {
