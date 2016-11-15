10a632fbf63fca7aab5795838e7b645d3cd53b5c
Roberto Loja <robertoloja@gmail.com>
2016-11-15 02:06:07 -0500

    Minor.

--- smartHive/src/main/java/nottoobee/toobee/smarthive/DatabaseHelper.java
+++ smartHive/src/main/java/nottoobee/toobee/smarthive/DatabaseHelper.java
@@ -1,5 +1,14 @@
 package nottoobee.toobee.smarthive;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DatabaseHelper {
+    private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+
+    public void addHive(String name, String location) {
+
+        DatabaseReference mUserRef = mRootRef.child("");
+    }
 }
