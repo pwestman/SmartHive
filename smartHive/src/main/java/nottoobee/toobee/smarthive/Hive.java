8b0fedf986eecc4a35056797a0bbbf04451c7324
Roberto Loja <robertoloja@gmail.com>
2016-12-11 22:24:55 -0500

    Made Hive and Data classes parcelable.

--- smartHive/src/main/java/nottoobee/toobee/smarthive/Hive.java
+++ smartHive/src/main/java/nottoobee/toobee/smarthive/Hive.java
@@ -4,6 +4,8 @@
 
 package nottoobee.toobee.smarthive;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import java.util.Date;
 
 /*
@@ -16,7 +18,7 @@ import java.util.Date;
     It is also possible to pull values from the database directly into a Hive object.
 */
 
-public class Hive {
+public class Hive implements Parcelable{
     private String name;
     private String location;
     private long date_created;
@@ -24,6 +26,13 @@ public class Hive {
     private String key;
 
     public Hive() {
+    }
+
+    public Hive(Parcel in) {
+        name = in.readString();
+        location = in.readString();
+        date_created = in.readLong();
+        data = in.readParcelable(Data.class.getClassLoader());
     }
 
     public Hive(String name, String location) {
@@ -48,11 +57,30 @@ public class Hive {
         return data;
     }
 
-    public String getKey() {
-        return key;
-    }
-
     public void setKey(String key) {
         this.key = (key);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(location);
+        dest.writeLong(date_created);
+        dest.writeParcelable(data, 0);
+    }
+
+    public static final Parcelable.Creator<Hive> CREATOR = new Parcelable.Creator<Hive>() {
+        public Hive createFromParcel(Parcel in) {
+            return new Hive(in);
+        }
+
+        public Hive[] newArray(int size) {
+            return new Hive[size];
+        }
+    };
 }
